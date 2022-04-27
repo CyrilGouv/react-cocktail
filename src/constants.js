5736b7955bfb06cd1aaead144f2df46cbce93130
Cyril Gouv <hello@cyrilgouv.com>
2022-04-27 11:45:03 +0200

    fetch single cocktail

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+export const API_URL_ID = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
--- src/pages/cocktail.js
+++ src/pages/cocktail.js
@@ -1,9 +1,81 @@
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import { FaAngleLeft } from 'react-icons/fa'
+import { API_URL_ID } from '../constants'
+
+import '../styles/Cocktail.css'
 
 
 const CocktailPage = () => {
-  return (
-    <section>cocktail</section>
-  )
+
+  const [drink, setDrink] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(API_URL_ID + id)
+      const data = await res.json()
+      const { drinks } = data
+
+      if (drinks[0]) {
+        const cocktails = drinks.map(drink => {
+          const { idDrink, strDrink, strDrinkThumb, strCategory, strGlass, strAlcoholic, strInstructions } = drink
+
+          const newDrink = {
+              _id: idDrink,
+              title: strDrink,
+              image: strDrinkThumb,
+              category: strCategory,
+              glass: strGlass,
+              alcoholic: strAlcoholic,
+              instructions: strInstructions,
+          }
+
+          return newDrink
+        })
+
+        setDrink(cocktails)
+        setLoading(false)
+      } else {
+        setDrink([])
+        setLoading(false)
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  
+  if (!loading && drink) {
+
+    const { _id, title, image, category, glass, alcoholic, instructions } = drink[0]
+  
+    return (
+      <section className="cocktail">
+        <div className="cocktail__img">
+          <img src={ image } alt={ title } />
+        </div>
+  
+        <div className="cocktail__content">
+          <Link to='/' className="cocktail__back"><FaAngleLeft /></Link>
+          <h1>{ title }</h1>
+          <p className="cocktail__desc">{ instructions }</p>
+          <p className="cocktail__options"><span>Category:</span>{ category }</p>
+          <p className="cocktail__options"><span>Info:</span>{ alcoholic }</p>
+          <p className="cocktail__options"><span>Glass:</span>{ glass }</p>
+        </div>
+      </section>
+    )
+  }
+
 }
 
 export default CocktailPage
